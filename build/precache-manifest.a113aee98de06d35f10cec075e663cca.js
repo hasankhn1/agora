f83f73947bc2fa9dc79501977008391949c99fb7
Hassan <h.karim@sunandsandsports.com>
2021-03-06 09:11:17 +0400

    agora styling bootstrap and fontawesome

---
+++ build/precache-manifest.a113aee98de06d35f10cec075e663cca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2f60c4b0f09060a184f3",
+    "url": "/static/js/main.2f60c4b0.chunk.js"
+  },
+  {
+    "revision": "d1d441e4240338532ac9",
+    "url": "/static/js/1.d1d441e4.chunk.js"
+  },
+  {
+    "revision": "2f60c4b0f09060a184f3",
+    "url": "/static/css/main.9738faed.chunk.css"
+  },
+  {
+    "revision": "d1d441e4240338532ac9",
+    "url": "/static/css/1.fd4de171.chunk.css"
+  },
+  {
+    "revision": "5c71d2d7bd0539d803e9d038553f29ae",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f56722ca4635a321535cbf3e0db6de21.js"
+  "/precache-manifest.a113aee98de06d35f10cec075e663cca.js"
 );
 
 workbox.clientsClaim();
